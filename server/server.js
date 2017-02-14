28cfb094443f82cefdbac7829af9554969aa787e
brianbuie <brianjamesbuie@gmail.com>
2017-02-13 22:18:12 -0600

    basic API with GET, COUNT, RAND endpoints, query params to narrow down results

--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ var cookieParser = require('cookie-parser');
 var credentials = require('./credentials.js');
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
+var Quotes = require('./models/theoffice/quotes.js');
 
 var app = express();
+mongoose.connect('mongodb://localhost/office');
 
 // security: disables info about server
 app.disable('x-powered-by');
@@ -17,12 +20,49 @@ app.use(cookieParser(credentials.cookieSecret));
 
 app.set('port', process.env.PORT || 8080);
 
+// make json pretty
+app.set('json spaces', 2);
+
 // static files
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.get('/', function(req, res){
-	res.send('Hello World');
+app.get('/api/', function(req, res){
+	Quotes.find(req.query, function(err, quotes){
+		if(err){ handleError(err); } 
+		else{ 
+			res.status(200).json(quotes);
+		}
+	});
 });
+
+app.get('/api/count/', function(req, res){
+	Quotes.count(req.query, function(err, quotes){
+		if(err){ handleError(err); } 
+		else{  
+			res.status(200).json(quotes);
+		}
+	});
+});
+
+app.get('/api/random/', function(req, res){
+	Quotes.count(req.query, function(err, count){
+		if(err){ handleError(err); } 
+		else{
+			let random = Math.floor(Math.random() * count);
+			Quotes.findOne(req.query).skip(random).exec(function(err, result){
+				if(err){ handleError(err); }
+				else{
+					res.status(200).json(result);
+				}
+			});
+		}
+	});
+});
+
+function handleError(err, res){
+	console.log(err);
+	res.status(500).json('Server error');
+}
 
 app.listen(app.get('port'), function(){
 	console.log('App running on port ' + app.get('port'));
